6817083dda62aaa87006ab8d2e8fc14afb864a77
Zhengyuan Wu <31089261+dqgo@users.noreply.github.com>
2024-08-12 14:57:45 +0800

    Delete src/common.rs

--- src/common.rs
+++
@@ -1,9 +0,0 @@
-#[derive(Debug)]
-pub struct Data {
-    change_data: Vec<Vec<i32>>,
-    job_num: usize,
-    work_num: usize,
-    factory_num: usize,
-    assembly: Vec<i32>,
-    assembly_data: Vec<i32>,
-}
