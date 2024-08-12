5f64a484a4b830c26d6d62c9805b0710ece8d1a2
Zhengyuan Wu <31089261+dqgo@users.noreply.github.com>
2024-08-12 14:59:33 +0800

    Delete src/find_schedule_question.m

--- src/find_schedule_question.m
+++
@@ -1,6 +0,0 @@
-% 新的schedule=[1工件号 2工序号  3机器号 4开工时间 5完工时间 6工厂号 7装配号 8属性(0加工/1装配) 9是否关键块 10UVQ标识 0=U 1=Q 2=V -1=其他 ]
-% schedule检查函数
-function is_schedule_ok = find_schedule_question(schedule)
-    is_schedule_ok = true;
-
-end
